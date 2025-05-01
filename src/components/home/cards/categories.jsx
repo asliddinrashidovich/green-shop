56f01f8ca0a87c4296507d417c31458c13168fc0
Asliddin <asliddinrashidovich7@gmail.com>
2025-05-01 11:55:25 +0500

    sorting filter updated

--- src/components/home/cards/categories.jsx
+++ src/components/home/cards/categories.jsx
@@ -3,18 +3,57 @@ import { useState } from "react"
 import MainButton from "../../button/button"
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useSearchParams } from "react-router-dom";
 
 function Categories() {
-  const [categoryBy, setCategoryBy] = useState('house-plants')
-  const [activeCategory, setActiveCategory] = useState('house-plants')
-  const [activeCapacity, setActiveCapacity] = useState('Small')
-  const [activeNew, setActiveNew] = useState('All Plants')
-  const [age, setAge] = useState('');
-  const [value, setValue] = useState([300, 660]);
+  const [age, setAge] = useState('default-sorting');
+  const [value, setValue] = useState([0, 1000]);
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+
+  // useSearchparams
+
+  // sort by category
+  const [searchParams, setSearchParams] = useSearchParams()
+  const categoryBy  = searchParams.get('category') || 'house-plants'
+  
+  const updateParams = (categorytype) => {
+    searchParams.set('category', categorytype)
+    setSearchParams(searchParams)
+    handleClose()
+  }
+
+  // sort by type
+  const typeSort  = searchParams.get('type') || 'all-plants'
+  
+  const updateTypeSort = (sortType) => {
+    searchParams.set('type', sortType)
+    setSearchParams(searchParams)
+  }
+  
+  // sort by sort
+  const sortBy  = searchParams.get('sort') || 'default-sorting'
+  
+  const updateSortBy = (sort) => {
+    setAge(sort);
+    searchParams.set('sort', sort)
+    setSearchParams(searchParams)
+  }
+  
+  // filter by amount
+  const rangeMin  = searchParams.get('range_min') || 0
+  const rangeMax  = searchParams.get('range_max') || 1000
+  
+  const handleSliderFilter = () => {
+    searchParams.set('range_min', value[0])
+    searchParams.set('range_max', value[1])
+    setSearchParams(searchParams)
+    handleClose()
+  }
+  
+  
     // getting APIs
     const fetchCategory = async () => {
       const res = await axios.get(`https://green-shop-backend.onrender.com/api/flower/category?access_token=6506e8bd6ec24be5de357927`);
@@ -24,8 +63,8 @@ function Categories() {
       const res = await axios.get(`https://green-shop-backend.onrender.com/api/features/discount?access_token=6506e8bd6ec24be5de357927`);
       return res.data;
     };
-    const fetchFlowersByCategory = async (categoryBy) => {
-      const res = await axios.get(`https://green-shop-backend.onrender.com/api/flower/category/${categoryBy}?access_token=6506e8bd6ec24be5de357927`);
+    const fetchFlowersByCategory = async ({categoryBy, typeSort, sortBy, rangeMin, rangeMax}) => {
+      const res = await axios.get(`https://green-shop-backend.onrender.com/api/flower/category/${categoryBy}?access_token=6506e8bd6ec24be5de357927&type=${typeSort}&sort=${sortBy}&range_min=${rangeMin}&range_max=${rangeMax}`);
       return res.data;
     };
 
@@ -38,21 +77,13 @@ function Categories() {
       queryFn: fetchDiscount,
     });
     const { data: flowersData } = useQuery({
-      queryKey: ["flowers", categoryBy],
-      queryFn: () => fetchFlowersByCategory(categoryBy),
+      queryKey: ["flowers", {categoryBy, typeSort, sortBy, rangeMin, rangeMax}],
+      queryFn: () => fetchFlowersByCategory({categoryBy, typeSort, sortBy, rangeMin, rangeMax}),
     });
 
-    const handleChange = (event) => {
-      setAge(event.target.value);
-    };
     const handleChangeSlider = (event, newValue) => {
       setValue(newValue);
     };
-    function handleActiveCategory(path) {
-      setCategoryBy(path)
-      setActiveCategory(path)
-      handleClose()
-    }
     // console.log(flowersData)
   return (
     <div className="max-w-[1200px] mx-auto flex gap-[50px] px-[20px] sm:px-[40px]">
@@ -60,39 +91,26 @@ function Categories() {
           <h2 className="font-[700] text-[18px] leading-[16px] mb-[7px]">Categories</h2>
           <div className="px-[12px] mb-[36px]">
             {categoryData?.data && categoryData?.data.map((item) => (
-              <div key={item.title} onClick={() => handleActiveCategory(item.route_path)} className="flex text-[#46A358] justify-between w-full cursor-pointer group">
-                <p className={`font-[700] text-[15px] leading-[40px] ${activeCategory == item.route_path ? 'text-[#46A358]' : 'text-[#3D3D3D] '}  group-hover:text-[#46A358] transition-all duration-[.3s]` }>{item.title}</p>
-                <p className={`font-[700] text-[15px] leading-[40px] ${activeCategory == item.route_path ? 'text-[#46A358]' : 'text-[#3D3D3D] '} group-hover:text-[#46A358] transition-all duration-[.3s]`}>({item.count})</p>
+              <div key={item.title} onClick={() => updateParams(item.route_path)} className="flex text-[#46A358] justify-between w-full cursor-pointer group">
+                <p className={`font-[700] text-[15px] leading-[40px] ${categoryBy == item.route_path ? 'text-[#46A358]' : 'text-[#3D3D3D] '}  group-hover:text-[#46A358] transition-all duration-[.3s]` }>{item.title}</p>
+                <p className={`font-[700] text-[15px] leading-[40px] ${categoryBy == item.route_path ? 'text-[#46A358]' : 'text-[#3D3D3D] '} group-hover:text-[#46A358] transition-all duration-[.3s]`}>({item.count})</p>
               </div>
             ))}
           </div>
           <h2 className="font-[700] text-[18px] leading-[16px] mb-[7px]">Price Range</h2>
           <div className="px-[12px] mb-[46px]">
-          <Slider
-            getAriaLabel={() => 'Temperature range'}
-            value={value}
-            onChange={handleChangeSlider}
-            valueLabelDisplay="auto"
-            max={1000}
-            style={{color: '#46A358'}}
-          />
-            <h2 className="font-[700] text-[15px] leading-[16px] mb-[16px]">Price: <span className="text-[#46A358]">${value[0]} - ${value[1]}</span></h2>
-            <MainButton>Filter</MainButton>
-          </div>
-          <h2 className="font-[700] text-[18px] leading-[16px] mb-[7px]">Categories</h2>
-          <div className="px-[12px] mb-[36px]">
-            <div onClick={() => setActiveCapacity('Small')} className="flex text-[#46A358] justify-between w-full cursor-pointer group">
-              <p className={`font-[700] text-[15px] leading-[40px] ${activeCapacity == 'Small' ? 'text-[#46A358]' : 'text-[#3D3D3D] '}  group-hover:text-[#46A358] transition-all duration-[.3s]` }>Small</p>
-              <p className={`font-[700] text-[15px] leading-[40px] ${activeCapacity == 'Small' ? 'text-[#46A358]' : 'text-[#3D3D3D] '} group-hover:text-[#46A358] transition-all duration-[.3s]`}>119</p>
-            </div>
-            <div onClick={() => setActiveCapacity('Medium')} className="flex text-[#46A358] justify-between w-full cursor-pointer group">
-              <p className={`font-[700] text-[15px] leading-[40px] ${activeCapacity == 'Medium' ? 'text-[#46A358]' : 'text-[#3D3D3D] '}  group-hover:text-[#46A358] transition-all duration-[.3s]` }>Medium</p>
-              <p className={`font-[700] text-[15px] leading-[40px] ${activeCapacity == 'Medium' ? 'text-[#46A358]' : 'text-[#3D3D3D] '} group-hover:text-[#46A358] transition-all duration-[.3s]`}>46</p>
-            </div>
-            <div onClick={() => setActiveCapacity('Large')} className="flex text-[#46A358] justify-between w-full cursor-pointer group">
-              <p className={`font-[700] text-[15px] leading-[40px] ${activeCapacity == 'Large' ? 'text-[#46A358]' : 'text-[#3D3D3D] '}  group-hover:text-[#46A358] transition-all duration-[.3s]` }>Large</p>
-              <p className={`font-[700] text-[15px] leading-[40px] ${activeCapacity == 'Large' ? 'text-[#46A358]' : 'text-[#3D3D3D] '} group-hover:text-[#46A358] transition-all duration-[.3s]`}>35</p>
-            </div>
+            <Slider
+              getAriaLabel={() => 'Temperature range'}
+              value={value}
+              onChange={handleChangeSlider}
+              valueLabelDisplay="auto"
+              max={1000}
+              style={{color: '#46A358'}}
+            />
+              <h2 className="font-[700] text-[15px] leading-[16px] mb-[16px]">Price: <span className="text-[#46A358]">${value[0]} - ${value[1]}</span></h2>
+              <button onClick={handleSliderFilter}>
+                <MainButton>Filter</MainButton>
+              </button>
           </div>
           <div className="pt-[22px] text-center bg-linear-to-br from-[#46A3581A] to-[#46A35808]">
             <h2 className="font-[400] text-[51px] leading-[65px] text-[#46A358] mb-[11px]">Super Sale</h2>
@@ -103,24 +121,24 @@ function Categories() {
         <div className="w-full lg:w-[70%]">
             <div className="flex justify-between items-center mb-[35px]">
               <div className="flex gap-[15px] md:gap-[35px]">
-                <h3 onClick={() => setActiveNew('All Plants')} className={`text-[14px] md:text-[16px] ${activeNew == 'All Plants' ? 'text-[#46A358] border-b-[1px] border-[#46A358] pb-[5px]' : 'text-[#3D3D3D] pb-[5px]'}  font-[700] cursor-pointer leading-[16px]`}>All Plants</h3>
-                <h3 onClick={() => setActiveNew('New Arrivals')} className={`text-[14px] md:text-[16px] ${activeNew == 'New Arrivals' ? 'text-[#46A358] border-b-[1px] border-[#46A358] pb-[5px]' : 'text-[#3D3D3D] pb-[5px]'}  font-[700] cursor-pointer leading-[16px]`}>New Arrivals</h3>
-                <h3 onClick={() => setActiveNew('Sale')} className={`text-[14px] md:text-[16px] ${activeNew == 'Sale' ? 'text-[#46A358] border-b-[1px] border-[#46A358] pb-[5px]' : 'text-[#3D3D3D] pb-[5px]'}  font-[700] cursor-pointer leading-[16px]`}>Sale</h3>
+                <h3 onClick={() => updateTypeSort('all-plants')} className={`text-[14px] md:text-[16px] ${typeSort == 'all-plants' ? 'text-[#46A358] border-b-[1px] border-[#46A358] pb-[5px]' : 'text-[#3D3D3D] pb-[5px]'}  font-[700] cursor-pointer leading-[16px]`}>All Plants</h3>
+                <h3 onClick={() => updateTypeSort('new-arrivals')} className={`text-[14px] md:text-[16px] ${typeSort == 'new-arrivals' ? 'text-[#46A358] border-b-[1px] border-[#46A358] pb-[5px]' : 'text-[#3D3D3D] pb-[5px]'}  font-[700] cursor-pointer leading-[16px]`}>New Arrivals</h3>
+                <h3 onClick={() => updateTypeSort('sale')} className={`text-[14px] md:text-[16px] ${typeSort == 'sale' ? 'text-[#46A358] border-b-[1px] border-[#46A358] pb-[5px]' : 'text-[#3D3D3D] pb-[5px]'}  font-[700] cursor-pointer leading-[16px]`}>Sale</h3>
               </div>
               <div className="lg:flex gap-[5px] hidden items-center">
-                <h2>Short by:</h2>
+                <h2>Sort by:</h2>
                 <FormControl sx={{ marginLeft: 1, minWidth: 120,  }}>
                   <Select
                     value={age}
-                    onChange={handleChange}
+                    onChange={(e) => updateSortBy(e.target.value)}
                     displayEmpty
                     inputProps={{ 'aria-label': 'Without label' }}
                   >
-                    <MenuItem value="">
+                    <MenuItem value="default-sorting">
                       <em>Default sorting</em>
                     </MenuItem>
-                    <MenuItem value={10}>The Cheapest</MenuItem>
-                    <MenuItem value={20}>The Expensive</MenuItem>
+                    <MenuItem value={'most-cheapest'}>Most Cheapest</MenuItem>
+                    <MenuItem value={"most-expensive"}>Most Expensive</MenuItem>
                   </Select>
                 </FormControl>
               </div>
@@ -133,9 +151,9 @@ function Categories() {
                     <h2 className="font-[700] text-[18px] leading-[16px] mb-[7px]">Categories</h2>
                     <div className="px-[12px] mb-[36px]">
                       {categoryData?.data && categoryData?.data.map((item) => (
-                        <div key={item.title} onClick={() => handleActiveCategory(item.route_path)} className="flex text-[#46A358] justify-between w-full cursor-pointer group">
-                          <p className={`font-[700] text-[15px] leading-[40px] ${activeCategory == item.route_path ? 'text-[#46A358]' : 'text-[#3D3D3D] '}  group-hover:text-[#46A358] transition-all duration-[.3s]` }>{item.title}</p>
-                          <p className={`font-[700] text-[15px] leading-[40px] ${activeCategory == item.route_path ? 'text-[#46A358]' : 'text-[#3D3D3D] '} group-hover:text-[#46A358] transition-all duration-[.3s]`}>({item.count})</p>
+                        <div key={item.title} onClick={() => updateParams(item.route_path)} className="flex text-[#46A358] justify-between w-full cursor-pointer group">
+                          <p className={`font-[700] text-[15px] leading-[40px] ${categoryBy == item.route_path ? 'text-[#46A358]' : 'text-[#3D3D3D] '}  group-hover:text-[#46A358] transition-all duration-[.3s]` }>{item.title}</p>
+                          <p className={`font-[700] text-[15px] leading-[40px] ${categoryBy == item.route_path ? 'text-[#46A358]' : 'text-[#3D3D3D] '} group-hover:text-[#46A358] transition-all duration-[.3s]`}>({item.count})</p>
                         </div>
                       ))}
                     </div>
@@ -150,9 +168,9 @@ function Categories() {
                       style={{color: '#46A358'}}
                     />
                       <h2 className="font-[700] text-[15px] leading-[16px] mb-[16px]">Price: <span className="text-[#46A358]">${value[0]} - ${value[1]}</span></h2>
-                      <div onClick={handleClose} className="inline-block">
+                      <button onClick={handleSliderFilter} className="inline-block">
                         <MainButton>Filter</MainButton>
-                      </div>
+                      </button>
                     </div>
                   </Box>
                 </Modal>
